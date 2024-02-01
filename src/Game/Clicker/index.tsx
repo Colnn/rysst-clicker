ae0685693f888313f82d6d7bd4a923cedbc1febc
_Coln <csoer1947@student.landstede.nl>
2024-02-01 20:28:21 +0100

    Something something

--- src/Game/Clicker/index.tsx
+++ src/Game/Clicker/index.tsx
@@ -1,12 +1,29 @@
-import { Box } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import useStyle from './style'
 
 export default function Clicker() {
     const classes = useStyle();
 
+    const onClick = () => {
+        console.log("Clicked :D");
+    }
+
     return (
         <>
-            <Box className={classes.container}>Clicker :D</Box>
+            <Grid container className={classes.container}>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    className={classes.riceContainer}
+                >
+                    <button className={classes.rice} onClick={onClick}>
+                        Click this :D
+                    </button>
+                </Grid>
+                <Box></Box>
+            </Grid>
         </>
     )
 }
--- src/Game/Clicker/style.ts
+++ src/Game/Clicker/style.ts
@@ -8,5 +8,22 @@ export default makeStyles(() => {
       height: '100%',
       backgroundColor: '#6f00ff',
     },
+    riceContainer: {
+      width: '100%',
+      height: '65%',
+    },
+    rice: {
+      border: '1px solid transparent',
+      padding: '0.6em 1.2em',
+      fontSize: '1em',
+      fontWeight: 500,
+      fontFamily: 'inherit',
+      cursor: 'pointer',
+      transition: 'borderColor 0.25s',
+      width: '35vh',
+      height: '35vh',
+      backgroundColor: '#fff',
+      borderRadius: '10px',
+    }
   };
 });
