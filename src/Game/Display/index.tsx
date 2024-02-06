a3a0118cee946e2159424acce2591be418f4cd80
Kaaspower <118729858+Kaaspower@users.noreply.github.com>
2024-02-06 10:20:19 +0100

    Yippieee :D

--- src/Game/Display/index.tsx
+++ src/Game/Display/index.tsx
@@ -1,14 +1,23 @@
 import { Box } from "@mui/material";
 import useStyle from './style'
 import MotivationalTexts from "../Components/motivationalTexts";
+import ShopObjectDisplay from "../Components/ShopObjectDisplay";
+import { useState } from "react";
 
 export default function Display() {
     const classes = useStyle();
+    const [amount, setAmount] = useState(5);
+
+    const weewoo = () => {
+        setAmount(amount + 1);
+    }
 
     return (
         <>
             <Box className={classes.container} >
                 <MotivationalTexts />
+                <ShopObjectDisplay amount={amount} objectName="Developer" backgroundSVG="/rysst_cooker.png" />
+                <button onClick={weewoo}>click me</button>
             </Box>
         </>
     )
