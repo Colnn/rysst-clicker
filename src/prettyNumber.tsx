e53d023fd7b985ee97dbe6397fa2231466dabebb
Colnn <csoer1947@student.landstede.nl>
2024-02-08 11:38:00 +0100

    Alsjeblieft René :)

--- src/prettyNumber.tsx
+++ src/prettyNumber.tsx
@@ -1,31 +1,29 @@
 export default function prettyNumber(nr: any, precision: number) {
-    if (nr >= 1e9) {
-        nr = nr / 1e9;
-        nr = nr.toFixed(precision);
-        if (nr[nr.length - 1] === '0') {
-            nr = nr.slice(0, -2);
-        }
-        return nr + ' trillion';
-      }
-      else if (nr >= 1e6) {
-      nr = nr / 1e6;
-      nr = nr.toFixed(precision);
-      if (nr[nr.length - 1] === '0') {
-          nr = nr.slice(0, -2);
-      }
-      return nr + ' million';
+  if (nr >= 1e9) {
+    nr = nr / 1e9;
+    nr = nr.toFixed(precision);
+    if (nr[nr.length - 1] === "0") {
+      nr = nr.slice(0, -2);
     }
-    else if (nr >= 1e3) {
-      nr = nr / 1e3;
-      nr = nr.toFixed(precision);
-      if (nr[nr.length - 1] === '0') {
-          nr = nr.slice(0, -2);
-      }
-      return nr + ' thousand';
+    return nr + " trillion";
+  } else if (nr >= 1e6) {
+    nr = nr / 1e6;
+    nr = nr.toFixed(precision);
+    if (nr[nr.length - 1] === "0") {
+      nr = nr.slice(0, -2);
     }
-    nr = nr.toFixed(2);
-    if (nr[nr.length - 1] === '0') {
-      nr = nr.slice(0, -3);
+    return nr + " million";
+  } else if (nr >= 1e3) {
+    nr = nr / 1e3;
+    nr = nr.toFixed(precision);
+    if (nr[nr.length - 1] === "0") {
+      nr = nr.slice(0, -2);
     }
-    return String(nr);
+    return nr + " thousand";
   }
+  nr = nr.toFixed(2);
+  if (nr[nr.length - 1] === "0") {
+    nr = nr.slice(0, -3);
+  }
+  return String(nr);
+}
