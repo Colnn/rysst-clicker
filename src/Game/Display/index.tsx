f988a89afa8fab2dcbe91f042741236e730fa891
Colnn <csoer1947@student.landstede.nl>
2024-02-13 09:03:52 +0100

    Updated file structure

--- src/Game/Display/index.tsx
+++ src/Game/Display/index.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import useStyle from './style';
-import MotivationalTexts from '../Components/motivationalTexts';
-import ShopObjectDisplay from '../Components/ShopObjectDisplay';
+import MotivationalTexts from './Components/motivationalTexts';
+import ShopObjectDisplay from './Components/ShopObjectDisplay';
 import { useEffect, useState } from 'react';
 import { defaultShopItems } from '..';
 
