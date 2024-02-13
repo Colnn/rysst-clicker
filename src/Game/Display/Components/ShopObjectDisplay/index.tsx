9a48dd615d90825d47071dcd65dadb719eac0914
Colnn <csoer1947@student.landstede.nl>
2024-02-13 13:45:10 +0100

    Little fixes after merging main

--- src/Game/Display/Components/ShopObjectDisplay/index.tsx
+++ src/Game/Display/Components/ShopObjectDisplay/index.tsx
@@ -5,69 +5,67 @@ import useStyle from './style';
 interface ShopObjectProps {
   amount: number;
   objectName: string;
-  backgroundSVG: string;
 }
 
 export default function ShopObjectDisplay({
   amount,
   objectName,
-  backgroundSVG,
 }: ShopObjectProps) {
   const classes = useStyle();
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
+  const drawImagesOnCanvas = () => {
+    if (!canvasRef.current || amount == 0) return;
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) return;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.canvas.width =
+      (document.getElementById('canvas')?.offsetWidth || 0);
+    ctx.canvas.height = 200;
+
+    ctx.imageSmoothingEnabled = false;
+
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < (canvas.width / 200); i++) {
+      const img = new Image();
+      img.src = `/${objectName.toLowerCase()}-background.png`;
+
+      img.onload = () => {
+        ctx.drawImage(img, x, y, 200, 200);
+
+        x += 200;
+      };
+    }
+
+    let x2 = 0;
+    let y2 = 10;
+
+    for (let i = 0; i < amount; i++) {
+      const img = new Image();
+      img.src = `/${objectName.toLowerCase() + i % 3}.png`;
+
+      img.onload = () => {
+        ctx.drawImage(img, x2, y2, 100, 100);
+
+        // Adjust x and y for the next image
+        x2 += 55; // You can adjust the spacing between images
+        if (y2 === 55) {
+          y2 = 10;
+        } else if (y2 == 10) {
+          y2 = 55;
+        }
+      };
+    }
+  };
+
   useEffect(() => {
-    const drawImagesOnCanvas = () => {
-      if (!canvasRef.current || amount == 0) return;
-
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-
-      if (!ctx) return;
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      ctx.canvas.width =
-        (document.getElementById('canvas')?.offsetWidth || 0);
-      ctx.canvas.height = 200;
-
-      ctx.imageSmoothingEnabled = false;
-
-      let x = 0;
-      let y = 0;
-
-      for (let i = 0; i < (canvas.width / 200); i++) {
-        const img = new Image();
-        img.src = `/${objectName.toLowerCase()}-background.png`;
-
-        img.onload = () => {
-          ctx.drawImage(img, x, y, 200, 200);
-
-          x += 200;
-        };
-      }
-
-      let x2 = 0;
-      let y2 = 10;
-
-      for (let i = 0; i < amount; i++) {
-        const img = new Image();
-        img.src = `/${objectName.toLowerCase() + i % 3}.png`;
-
-        img.onload = () => {
-          ctx.drawImage(img, x2, y2, 100, 100);
-
-          // Adjust x and y for the next image
-          x2 += 55; // You can adjust the spacing between images
-          if (y2 === 55) {
-            y2 = 10;
-          } else if (y2 == 10) {
-            y2 = 55;
-          }
-        };
-      }
-    };
-
     drawImagesOnCanvas();
   }, [amount]);
 
--- src/Game/Display/index.tsx
+++ src/Game/Display/index.tsx
@@ -2,7 +2,7 @@ import { Box, Grid, ToggleButton, ToggleButtonGroup, Tooltip, TooltipProps, Typo
 import useStyle from './style';
 import MotivationalTexts from './Components/MotivationalTexts';
 import ShopObjectDisplay from './Components/ShopObjectDisplay';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { DateTime } from 'luxon';
 import GrainsIndicator from '../Components/GrainsIndicator';
 import UpgradeTooltip from '../Shop/Components/UpgradeTooltip';
@@ -58,90 +58,89 @@ export default function Display({ shopData, upgradeData, spentGrains, collectedG
       <Grid container className={classes.container} direction={'row'} wrap={'nowrap'}>
         <Grid className={classes.innerContainer}>
           <ToggleButtonGroup className={classes.topContainer} value={page} onChange={changePage}>
-          <Grid
-            className={classes.topContainer}
-            container
-            direction={'row'}
-            justifyContent={'space-between'}
-          >
-            <Grid className={classes.options}>
-              <ToggleButton value={1} fullWidth>
-                Stats
-              </ToggleButton>
-            </Grid>
-            <MotivationalTexts />
+            <Grid
+              className={classes.topContainer}
+              container
+              direction={'row'}
+              justifyContent={'space-between'}
+            >
               <Grid className={classes.options}>
-              <ToggleButton value={1} fullWidth>
-                Test
-              </ToggleButton>
-            </Grid>
-          </Grid>
-        </ToggleButtonGroup>
-        {page == 0 &&
-          shopData.map((item) => {
-              return (
-                <ShopObjectDisplay
-                  key={item.name}
-                  amount={item.amount}
-                  objectName={item.name}
-                  backgroundSVG="/rysst_cooker.png"
-                />
-              );
-            })}
-          {page == 1 && (
-          <Grid container direction={'column'}>
-            <Grid container direction={'column'}>
-              <Box className={classes.statsHeader}><Typography variant='h5'>General</Typography></Box>
-              <Grid container direction={'row'} alignItems={'center'} wrap={'nowrap'}><Typography>RYSST-grains sold: </Typography><GrainsIndicator variant={"default"} value={spentGrains} precision={3}/></Grid>
-              <Grid container direction={'row'} alignItems={'center'} wrap={'nowrap'}><Typography>RYSST-grains cooked: </Typography><GrainsIndicator variant={"default"} value={collectedGrains} precision={3}/></Grid>
-              <Box>Run started: {dateStarted.toFormat('dd-MM-yyyy HH:mm')}</Box>
-              <Box>RYSST-grains per second: {0}</Box>
-              <Box>RYSST-grains per click: {0}</Box>
-            </Grid>
-            <Grid container direction={'column'}>
-              <Box className={classes.statsHeader}><Typography variant='h5'>Upgrades</Typography></Box>
-              <Grid container direction={'row'}>
-                {upgradeData.map((upgrade) => {
-                  if(!upgrade.unlocked) return;
-                  return (
-                    <>
-                      <HtmlTooltip
-                        title={
-                          <UpgradeTooltip
-                            name={upgrade.name}
-                            icon={'/' + upgrade.name.toLowerCase().replace(' ', '_') + '.png'}
-                            price={upgrade.price}
-                            disabled={false}
-                          />
-                        }
-                        placement="top"
-                        slotProps={{
-                          popper: {
-                            modifiers: [
-                              {
-                                name: 'offset',
-                                options: {
-                                  offset: [0, -4],
-                                },
-                              },
-                            ],
-                          },
-                        }}
-                      >
-                        <Box
-                          className={classes.upgradeContainer}
-                        >
-                          <Box className={classes.icon} component={'img'} src={'/' + upgrade.name.toLowerCase().replace(' ', '_') + '.png'} draggable={false} />
-                        </Box>
-                      </HtmlTooltip>
-                    </>
-                  )
-                })}
+                <ToggleButton value={1} fullWidth>
+                  Stats
+                </ToggleButton>
+              </Grid>
+              <MotivationalTexts />
+                <Grid className={classes.options}>
+                <ToggleButton value={1} fullWidth>
+                  Test
+                </ToggleButton>
               </Grid>
             </Grid>
-          </Grid>
-        )}
-      </Grid>
+          </ToggleButtonGroup>
+          {page == 0 &&
+            shopData.map((item) => {
+                return (
+                  <ShopObjectDisplay
+                    key={item.name}
+                    amount={item.amount}
+                    objectName={item.name}
+                  />
+                );
+              })}
+            {page == 1 && (
+            <Grid container direction={'column'}>
+              <Grid container direction={'column'}>
+                <Box className={classes.statsHeader}><Typography variant='h5'>General</Typography></Box>
+                <Grid container direction={'row'} alignItems={'center'} wrap={'nowrap'}><Typography>RYSST-grains sold: </Typography><GrainsIndicator variant={"default"} value={spentGrains} precision={3}/></Grid>
+                <Grid container direction={'row'} alignItems={'center'} wrap={'nowrap'}><Typography>RYSST-grains cooked: </Typography><GrainsIndicator variant={"default"} value={collectedGrains} precision={3}/></Grid>
+                <Box>Run started: {dateStarted.toFormat('dd-MM-yyyy HH:mm')}</Box>
+                <Box>RYSST-grains per second: {0}</Box>
+                <Box>RYSST-grains per click: {0}</Box>
+              </Grid>
+              <Grid container direction={'column'}>
+                <Box className={classes.statsHeader}><Typography variant='h5'>Upgrades</Typography></Box>
+                <Grid container direction={'row'}>
+                  {upgradeData.map((upgrade) => {
+                    if(!upgrade.unlocked) return;
+                    return (
+                      <>
+                        <HtmlTooltip
+                          title={
+                            <UpgradeTooltip
+                              name={upgrade.name}
+                              icon={'/' + upgrade.name.toLowerCase().replace(' ', '_') + '.png'}
+                              price={upgrade.price}
+                              disabled={false}
+                            />
+                          }
+                          placement="top"
+                          slotProps={{
+                            popper: {
+                              modifiers: [
+                                {
+                                  name: 'offset',
+                                  options: {
+                                    offset: [0, -4],
+                                  },
+                                },
+                              ],
+                            },
+                          }}
+                        >
+                          <Box
+                            className={classes.upgradeContainer}
+                          >
+                            <Box className={classes.icon} component={'img'} src={'/' + upgrade.name.toLowerCase().replace(' ', '_') + '.png'} draggable={false} />
+                          </Box>
+                        </HtmlTooltip>
+                      </>
+                    )
+                  })}
+                </Grid>
+              </Grid>
+            </Grid>
+          )}
+        </Grid>
         <Seperator/>
       </Grid>
     </>
