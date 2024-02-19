a80725a273db835f38af52e621579de69b7620d9
Colnn <csoer1947@student.landstede.nl>
2024-02-19 09:49:27 +0100

    Hwhat

---
+++ src/Game/Display/Components/MotivationalTexts/style.ts
@@ -0,0 +1,14 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => {
+  return {
+    motivationalTexts: {
+      Display: 'flex',
+      justifyContent: 'center',
+      padding: 20,
+      textAlign: 'center',
+      height: 90,
+      width: '80%',
+    },
+  };
+});
